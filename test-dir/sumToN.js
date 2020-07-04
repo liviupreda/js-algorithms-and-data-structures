f453a95b4cca1b9554edf772d6e2cc2c396e6322
Liviu Preda <liviupreda@gmail.com>
2020-07-04 23:28:05 +0300

    improve time performance

--- test-dir/sumToN.js
+++ test-dir/sumToN.js
@@ -1,11 +1,7 @@
 const { performance } = require("perf_hooks");
 
 function sumToN(n) {
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
-    sum += i;
-  }
-  return sum;
+  return (n * (n + 1)) / 2;
 }
 
 let time1 = performance.now();
