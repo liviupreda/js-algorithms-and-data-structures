1fcfe70746ee24e96ccd163a5e344a518eaa8a0a
Liviu Preda <liviupreda@gmail.com>
2020-07-04 23:26:31 +0300

    add performance measurement

--- test-dir/sumToN.js
+++ test-dir/sumToN.js
@@ -1,9 +1,14 @@
-function sumOfN(n) {
+const { performance } = require("perf_hooks");
+
+function sumToN(n) {
   let sum = 0;
-  for (let i = 0; i < n; i++) {
+  for (let i = 1; i <= n; i++) {
     sum += i;
   }
   return sum;
 }
 
-console.log(sumOfN(6));
+let time1 = performance.now();
+sumToN(1000000000);
+let time2 = performance.now();
+console.log(`Time performance: ${(time2 - time1) / 1000} seconds`);
