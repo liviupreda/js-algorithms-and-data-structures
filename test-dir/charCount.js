e0e1cc9a5b1d7d3e4200c63ab7415403c1ce13a7
Liviu Preda <liviupreda@gmail.com>
2020-07-11 17:00:18 +0300

    finish char count 1st solution

--- test-dir/charCount.js
+++ test-dir/charCount.js
@@ -4,13 +4,26 @@
 function charCount(inputString) {
   const validCharacters = "abcdefghijklmnopqrstuvwxyz".split("");
   const inputStringToArr = inputString.split("");
-  console.log(inputStringToArr);
+  const charCounter = [];
+  for (let i = 0; i < validCharacters.length; i++) {
+    charCounter.push(0);
+  }
+  console.log(charCounter);
   for (let i = 0; i < validCharacters.length; i++) {
     for (let j = 0; j < inputStringToArr.length; j++) {
-      if (inputStringToArr[j] === validCharacters[i])
+      if (inputStringToArr[j] === validCharacters[i]) {
         console.log(inputStringToArr[j]);
+        charCounter[i]++;
+      }
     }
+  }
+  console.log(charCounter);
+  for (let i = 0; i < charCounter.length; i++) {
+    if (charCounter[i] > 0)
+      console.log(
+        `Character ${validCharacters[i]} appears ${charCounter[i]} times`
+      );
   }
 }
 
-charCount("abcd");
+charCount("abcdadcb");
