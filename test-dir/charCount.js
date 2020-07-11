c50c0f616e470271f2bbe91c15fe4b454a44c325
Liviu Preda <liviupreda@gmail.com>
2020-07-11 17:24:58 +0300

    add type checking and error message if input is not a string

--- test-dir/charCount.js
+++ test-dir/charCount.js
@@ -2,25 +2,31 @@
 
 // "abcd"
 function charCount(inputString) {
-  const validCharacters = "abcdefghijklmnopqrstuvwxyz".split("");
-  const inputStringToArr = inputString.split("");
-  const charCounter = [];
-  for (let i = 0; i < validCharacters.length; i++) {
-    charCounter.push(0);
-  }
-  for (let i = 0; i < validCharacters.length; i++) {
-    for (let j = 0; j < inputStringToArr.length; j++) {
-      if (inputStringToArr[j].toLowerCase() === validCharacters[i]) {
-        charCounter[i]++;
+  if (typeof inputString !== "string") {
+    console.log("Please enter a valid string containing letters");
+    return;
+  } else {
+    const validCharacters = "abcdefghijklmnopqrstuvwxyz".split("");
+    const inputStringToArr = inputString.split("");
+    const charCounter = [];
+    for (let i = 0; i < validCharacters.length; i++) {
+      charCounter.push(0);
+    }
+    for (let i = 0; i < validCharacters.length; i++) {
+      for (let j = 0; j < inputStringToArr.length; j++) {
+        if (inputStringToArr[j].toLowerCase() === validCharacters[i]) {
+          charCounter[i]++;
+        }
       }
     }
-  }
-  for (let i = 0; i < charCounter.length; i++) {
-    if (charCounter[i] > 0)
-      console.log(
-        `Character ${validCharacters[i]} appears ${charCounter[i]} times`
-      );
+    for (let i = 0; i < charCounter.length; i++) {
+      if (charCounter[i] > 0)
+        console.log(
+          `Character ${validCharacters[i]} appears ${charCounter[i]} times`
+        );
+    }
   }
 }
 
-charCount("abcdadcb");
+charCount(1);
+charCount("Hello wohhrlddd!");
