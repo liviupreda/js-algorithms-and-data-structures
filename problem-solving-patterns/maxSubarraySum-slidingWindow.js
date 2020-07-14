50010ea9737a6b5b73a0b824664f998ba898fbe5
Liviu Preda <liviupreda@gmail.com>
2020-07-14 11:42:19 +0300

    refactor with improved solution

--- problem-solving-patterns/maxSubarraySum-slidingWindow.js
+++ problem-solving-patterns/maxSubarraySum-slidingWindow.js
@@ -10,19 +10,37 @@ maxSubarraySum([4, 2, 1, 6, 2], 4) // 13
 maxSubarraySum([], 4) // null
 
 Naive time complexity O(n^2)
+Refactor time complexity O(n)
 */
 
-function maxSubarraySumNaive(arr, num) {
-  if (num > arr.length) return null;
-  let max = -Infinity;
-  for (let i = 0; i < arr.length - num + 1; i++) {
-    let temp = 0;
-    for (let j = 0; j < num; j++) {
-      temp += arr[i + j];
-    }
-    if (temp > max) max = temp;
+// function maxSubarraySumNaive(arr, num) {
+//   if (num > arr.length) return null;
+//   let max = -Infinity;
+//   for (let i = 0; i < arr.length - num + 1; i++) {
+//     let temp = 0;
+//     for (let j = 0; j < num; j++) {
+//       temp += arr[i + j];
+//     }
+//     if (temp > max) max = temp;
+//   }
+//   return max;
+// }
+
+// console.log(maxSubarraySumNaive([1, 2, 5, 2, 8, 1, 5], 4));
+
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
   }
-  return max;
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
 }
 
-console.log(maxSubarraySumNaive([1, 2, 5, 2, 8, 1, 5], 4));
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2));
