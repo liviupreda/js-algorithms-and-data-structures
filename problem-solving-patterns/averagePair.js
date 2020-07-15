5c62029d525297b01990988330139b93aa193268
Liviu Preda <liviupreda@gmail.com>
2020-07-15 13:07:37 +0300

    finish solve average pair problem

--- problem-solving-patterns/averagePair.js
+++ problem-solving-patterns/averagePair.js
@@ -9,15 +9,16 @@ Space O(1)
 */
 
 function averagePair(arr, avg) {
-  let left = 0;
-  let right = left + 1;
+  let mid = Math.floor(arr.length / 2);
+  let left = mid;
+  let right = mid + 1;
   do {
     const test = (arr[left] + arr[right]) / 2;
     if (test === avg) return true;
-    left++;
-    right++;
-  } while (left < right && right < arr.length);
+    if (test < avg) right++;
+    if (test > avg) left--;
+  } while (left >= 0 && right < arr.length);
   return false;
 }
 
-console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8));
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); //true
