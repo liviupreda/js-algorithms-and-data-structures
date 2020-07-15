964f9191793735c2fe1d5bbee1436981da059ba2
Liviu Preda <liviupreda@gmail.com>
2020-07-15 11:58:42 +0300

    finish solve are there duplicates problem

---
+++ problem-solving-patterns/areThereDuplicates.js
@@ -0,0 +1,21 @@
+/*
+function accepts a variable number of arguments
+checks wether there are any duplicates among the arguments passed in
+DONE: Complexity: time O(n), space O(n)
+Bonus: Complexity time O(nlogn), space O(1)
+*/
+
+function areThereDuplicates(...args) {
+  console.log(args);
+  let obj = {};
+  for (let arg of args) {
+    obj[arg] = (obj[arg] || 0) + 1;
+  }
+  let keys = Object.keys(obj);
+  for (let key of keys) {
+    if (obj[key] > 1) return true;
+  }
+  return false;
+}
+
+console.log(areThereDuplicates("a", "b", "c", "d"));
