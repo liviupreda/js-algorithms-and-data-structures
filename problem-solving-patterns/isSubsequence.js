ba7e85628b9cff2f55ee54c5559fd84496051b26
Liviu Preda <liviupreda@gmail.com>
2020-07-16 12:43:43 +0300

    start work on is string subsequence problem

---
+++ problem-solving-patterns/isSubsequence.js
@@ -0,0 +1,17 @@
+/*
+Write a function called isSubsequence which takes in two strings
+and checks wether the characters in the first string form a subsequence
+of the characters in the second string. In other words, the function
+should check whether the characters in the first string appear somewhere
+in the second string, without their order changing
+
+Examples:
+isSubsequence('hello', 'hello world'); //true
+isSubsequence('sing', 'sting'); // true
+isSubsequence('abc', 'abracadabra'); // true
+isSubsequence('abc', 'acb'); //false (order matters)
+*/
+
+function isSubsequence() {}
+
+console.log(isSubsequence("hello", "hello world"));
