bc30bfc283ae8243203e68bd48273da54ee95e78
Liviu Preda <liviupreda@gmail.com>
2020-07-27 12:16:21 +0300

    finish solve max sub array problem

--- problem-solving-patterns/maxSubarraySum.js
+++ problem-solving-patterns/maxSubarraySum.js
@@ -18,4 +18,20 @@ Time complexity O(N)
 Space complexity O(1)
 */
 
-function maxSubarraySum() {}
+function maxSubarraySum(arr, num) {
+  if (arr.length < num) return null;
+
+  let total = 0;
+  for (let i = 0; i < num; i++) {
+    total += arr[i];
+  }
+  console.log(total);
+  let currentTotal = total;
+  for (let i = num; i < arr.length; i++) {
+    currentTotal += arr[i] - arr[i - num];
+    total = Math.max(total, currentTotal);
+  }
+  return total;
+}
+
+console.log(maxSubarraySum([100, 200, 300, 400], 2));
